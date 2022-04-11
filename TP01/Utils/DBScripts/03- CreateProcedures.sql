ac72ff8f20a1e269e406a875822346e07b76a762
Android Studio <devandroid@ort>
2022-04-11 09:52:15 -0300

    correccion, agregado el TP01

---
+++ TP01/Utils/DBScripts/03- CreateProcedures.sql
@@ -0,0 +1,36 @@
+CREATE PROCEDURE GetAll
+AS
+    SELECT * FROM [DAI-Pizzas].dbo.Pizzas
+GO;
+
+CREATE PROCEDURE GetByID @id INT
+AS
+    SELECT *
+    FROM [DAI-Pizzas].dbo.Pizzas
+    WHERE Pizzas.ID = @id
+GO;
+
+CREATE PROCEDURE CreatePizza @nombre VARCHAR(150), @libreGluten BIT, @importe FLOAT, @descripcion VARCHAR(max)
+AS
+    INSERT [DAI-Pizzas].dbo.[Pizzas]
+        ([Nombre], [LibreGluten], [Importe], [Descripcion])
+    VALUES
+        (@nombre, @libreGluten, @importe, @descripcion)
+GO;
+
+CREATE PROCEDURE DeletePizza @id INT
+AS
+    DELETE FROM [DAI-Pizzas].dbo.Pizzas
+    WHERE Pizzas.Id=@id
+GO;
+
+CREATE PROCEDURE UpdatePizza @nombre VARCHAR(150), @libreGluten BIT, @importe FLOAT, @descripcion VARCHAR(max), @id INT
+AS
+    UPDATE [DAI-Pizzas].dbo.Pizzas
+    SET
+        Nombre = @nombre,
+        LibreGluten = @libreGluten,
+        Importe = @importe,
+        Descripcion = @descripcion
+    WHERE Id = @id
+GO;
